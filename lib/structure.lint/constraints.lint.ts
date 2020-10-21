a761102d3e08911196652486d33e0afac94415ea
softmarshmallow <woojoo@softmarshmallow.com>
2020-10-21 20:39:59 +0900

    updated lint to depend on design-sdk

--- lib/structure.lint/constraints.lint.ts
+++ lib/structure.lint/constraints.lint.ts
@@ -1,13 +1,22 @@
-import { getgroups } from "process"
 import { LintResults, ReflectLintFeedback } from "../feedbacks/feedback"
 import { MissingConstraintsWarning } from "../feedbacks/constraints.feedback"
+import { ReflectSceneNode } from "@bridged.xyz/design-sdk/lib/nodes"
+import { getLCRS, LCRS } from "@bridged.xyz/design-sdk/lib/utils/lcrs"
+
+
+type Lint = boolean | LintResult
+
+interface LintResult {
+    error: Error
+}
+
 
 /**
  * Iterates throught the children, finds the missing constraints. It does not inspect the givven node itself.
  * Again, It inspects the children of the givven node, not the node itself.
  * @param node the target node to be inspected. node should be Group or Frame to be inspected.
  */
-export function lintMissingConstraints(node: SceneNode): LintResults {
+export function lintMissingConstraints(node: ReflectSceneNode): LintResults {
     const lints: Array<ReflectLintFeedback> = []
 
     const containerWidth = node.width
@@ -19,6 +28,7 @@ export function lintMissingConstraints(node: SceneNode): LintResults {
             const validTarget = childNode.visible && !childNode.locked
             if (validTarget) {
 
+                // FIXME rel does not work with group as expected.
                 const relX = childNode.x
                 const relXCenter = relX + (childNode.width / 2)
                 const relY = childNode.y
@@ -33,7 +43,7 @@ export function lintMissingConstraints(node: SceneNode): LintResults {
 
                 // INSTANCE, COMPONENT, FRAME are supported. GROUP support is blocked by https://github.com/figma/plugin-typings/issues/9
                 if (childNode.type == "INSTANCE" || childNode.type == "COMPONENT" || childNode.type == "FRAME" || childNode.type == "RECTANGLE" || childNode.type == "GROUP") {
-                    const xAlign: LCRS = getNodeLCRS(childNode);
+                    const xAlign: LCRS = node.lcrs
 
                     console.warn(node.name, xAlign)
                     switch (lcr) {
@@ -68,115 +78,4 @@ export function lintMissingConstraints(node: SceneNode): LintResults {
     } else {
         return true
     }
-}
-
-function getRelX(node: SceneNode) {
-    if (node.parent.type == "GROUP") {
-
-    }
-    node.x
-}
-
-function getRelY(node: SceneNode) {
-
-}
-
-function getNodeLCRS(node: SceneNode): LCRS {
-    if (node.type == "INSTANCE" || node.type == "COMPONENT" || node.type == "FRAME" || node.type == "RECTANGLE") {
-        return X_ALIGN_FIGMA_TO_REFLECT.get(node.constraints.horizontal)
-    } else if (node.type == "GROUP") {
-        return getGroupLCRS(node);
-    }
-}
-
-/**
- * check group's constraint
- * https://github.com/figma/plugin-typings/issues/9
- * @param node 
- */
-function getGroupLCRS(node: SceneNode): LCRS {
-    let lastLCRS: LCRS;
-    if (node.type == "GROUP") {
-        for (const c of node.children) {
-            const lcrs = getGroupLCRS(c)
-            if (lastLCRS) {
-                if (lastLCRS === lcrs) {
-                    // do nothing if lcrs matches
-                } else {
-                    // return mixed, if last lcrs does not match current one.
-                    return "Mixed"
-                }
-            } else {
-                lastLCRS = lcrs
-            }
-        }
-        return lastLCRS
-    } else {
-        return getNodeLCRS(node)
-    }
-}
-
-const X_ALIGN_FIGMA_TO_REFLECT: Map<ConstraintType, LCRS> = new Map([
-    ["MIN", "Left"],
-    ["MAX", "Right"],
-    ["CENTER", "Center"],
-    ["STRETCH", "Stretch"],
-    ["SCALE", "Scale"]
-])
-
-
-
-
-type Lint = boolean | LintResult
-
-interface LintResult {
-    error: Error
-}
-
-
-
-const SAFE_DAMPING_PX = 0.5
-type LCRS = "Left" | "Center" | "Right" | "Stretch" | "Scale" | "Mixed"
-/**
- * calculate the position element in X plane of givven container
- * 
- * |L--------|
- * 
- * |----C----|
- * 
- * |--------R|
- * 
- * the safe margin in non even numbers of PXs are dampped with @const SAFE_DAMPING_PX
- * @param containerWidth 
- * @param centerPosition 
- */
-function getLCRS(args: {
-    containerWidth: number, centerPosition: number, startPosition: number, width: number
-}): LCRS {
-    const { containerWidth, centerPosition, startPosition, width } = args
-
-    // stretch inspection
-    // if the size of child is same as parent, and start point is starting from 0, inspect it as "Stretch"
-    if (containerWidth == width && startPosition == 0) {
-        return "Stretch"
-    }
-
-
-
-    const isContainerWidthEven = containerWidth % 2 == 0
-    const isCenterPositionEven = centerPosition % 2 == 0
-
-    /** if one of the givven parameter is not a even number, than apply damping rule with @const SAFE_DAMPING_PX */
-    const damp = isContainerWidthEven && isCenterPositionEven ? 0 : SAFE_DAMPING_PX
-
-    const centerDiff = Math.abs((containerWidth / 2) - centerPosition)
-    if (centerDiff + damp < 0) {
-        // this is visually on the left side
-        return "Left"
-    } else if (centerDiff - damp > 0) {
-        // this is visually on the right side
-        return "Right"
-    }
-    // this is visually on the center
-    return "Center"
 }
