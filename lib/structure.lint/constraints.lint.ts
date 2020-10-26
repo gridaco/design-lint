a2e2c0264c531af58f8dbf4f550a972f3623ee40
softmarshmallow <woojoo@softmarshmallow.com>
2020-10-26 21:07:45 +0900

    fixed constraint matching

--- lib/structure.lint/constraints.lint.ts
+++ lib/structure.lint/constraints.lint.ts
@@ -17,43 +17,46 @@ interface LintResult {
  * @param node the target node to be inspected. node should be Group or Frame to be inspected.
  */
 export function lintMissingConstraints(node: ReflectConstraintMixin): LintResults {
+    console.log(`start linting for missing constraints on node ${node.toString()}`)
     const lints: Array<ReflectLintFeedback> = []
 
     if (node instanceof ReflectChildrenMixin) {
+        const parent = node.copyAsSnippet()
+
+
         for (const childNode of node.children) {
             const validTarget = childNode.visible && !childNode.locked
             if (validTarget) {
 
-                const lcr = childNode.relativeLcrs
+                const visualPositioning = childNode.relativeLcrs
 
                 // INSTANCE, COMPONENT, FRAME are supported. GROUP support is blocked by https://github.com/figma/plugin-typings/issues/9
                 if (childNode.type == "INSTANCE" || childNode.type == "COMPONENT" || childNode.type == "FRAME" || childNode.type == "RECTANGLE" || childNode.type == "GROUP") {
-                    const xAlign: LCRS = node.lcrs
+                    const xAlign: LCRS = childNode.constraintLcrs
                     const target = childNode.copyAsSnippet()
-                    const parent = node.copyAsSnippet()
-                    console.warn(node.name, xAlign)
-                    switch (lcr) {
+                    console.warn(childNode.name, visualPositioning, xAlign)
+                    switch (visualPositioning) {
                         case "Left":
                             if (!(xAlign == "Left")) {
-                                const warn = new MissingConstraintsWarning(target, parent, lcr, xAlign, lcr)
+                                const warn = new MissingConstraintsWarning(target, parent, visualPositioning, xAlign, visualPositioning)
                                 lints.push(warn)
                             }
                             break;
                         case "Right":
                             if (!(xAlign == "Right")) {
-                                const warn = new MissingConstraintsWarning(target, parent, lcr, xAlign, lcr)
+                                const warn = new MissingConstraintsWarning(target, parent, visualPositioning, xAlign, visualPositioning)
                                 lints.push(warn)
                             }
                             break;
                         case "Center":
                             if (!(xAlign == "Center" || xAlign == "Stretch")) {
-                                const warn = new MissingConstraintsWarning(target, parent, lcr, xAlign, `Stretch or Center`)
+                                const warn = new MissingConstraintsWarning(target, parent, visualPositioning, xAlign, `Stretch or Center`)
                                 lints.push(warn)
                             }
                             break;
                         case "Stretch":
                             if (!(xAlign == "Center" || xAlign == "Stretch" || xAlign == "Scale")) {
-                                const warn = new MissingConstraintsWarning(target, parent, lcr, xAlign, `Stretch, Center, or Scale`)
+                                const warn = new MissingConstraintsWarning(target, parent, visualPositioning, xAlign, `Stretch, Center, or Scale`)
                                 lints.push(warn)
                             }
                     }
