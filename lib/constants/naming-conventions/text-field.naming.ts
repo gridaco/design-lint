2a40ce959bb9ae43dbd22fc13530d97f0cdd77f5
softmarshmallow <woojoo@softmarshmallow.com>
2020-11-09 18:26:50 +0900

    naming glob patterns updated to match all

---
+++ lib/constants/naming-conventions/text-field.naming.ts
@@ -0,0 +1,15 @@
+/**
+ * TextField name pattern templates with Glob pattern
+ */
+export const DEFAULT_TEXT_FIELD_NAMING_CONVENTION_PATTERNS = [
+    "**/text field/**",
+    "**/fext-field/**",
+    "**/fextfield/**",
+    "**/text-field/**",
+    "**/text-fields/**",
+    "**/textfield/**",
+    "**/textfields/**",
+    "text field/**",
+    "textfield/**",
+    "textfields/**",
+]
