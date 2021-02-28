332ddb0c27253359dd638fda5da7356e54a6eadd
softmarshmallow <woojoo@softmarshmallow.com>
2021-03-01 02:09:58 +0900

    add more linting policies

--- lib/feedbacks/style.feedback.ts
+++ lib/feedbacks/style.feedback.ts
@@ -2,7 +2,17 @@ import { IReflectNodeReference } from "@bridged.xyz/design-sdk/lib/nodes";
 import { ReflectLintFeedback } from ".";
 
 export class MissingStyleFeedback extends ReflectLintFeedback {
-    constructor(target: IReflectNodeReference, message: string) {
-        super(target, message)
-    }
+  constructor(target: IReflectNodeReference, message: string) {
+    super(target, message);
+  }
+}
+
+/**
+ * Base MissingColorStyle feedback. feedbacks such as missing TextColorStyleFeedback extends this.
+ * This also has a general usage, this can be used as-is feedback as a root feedback. means: returning this as a feedback is fine.
+ */
+export class MissingColorStyleFeedback extends MissingStyleFeedback {
+  constructor(target: IReflectNodeReference, message: string) {
+    super(target, message);
+  }
 }
--- lib/feedbacks/text-style.feedback.ts
+++ lib/feedbacks/text-style.feedback.ts
@@ -1,8 +1,17 @@
 import { IReflectNodeReference } from "@bridged.xyz/design-sdk/lib/nodes";
-import { MissingStyleFeedback } from "./style.feedback";
+import {
+  MissingColorStyleFeedback,
+  MissingStyleFeedback,
+} from "./style.feedback";
 
 export class MissingTextStyleWarning extends MissingStyleFeedback {
-    constructor(target: IReflectNodeReference) {
-        super(target, `missing text style on text node "${target.name}"`)
-    }
+  constructor(target: IReflectNodeReference) {
+    super(target, `missing text style on text node "${target.name}"`);
+  }
+}
+
+export class MissingTextColorStyleWarning extends MissingColorStyleFeedback {
+  constructor(target: IReflectNodeReference) {
+    super(target, `missing text color style on text node "${target.name}"`);
+  }
 }
