49427d80dd49f45821624b7653b2b9498918815b
softmarshmallow <woojoo@softmarshmallow.com>
2021-08-17 00:35:31 +0900

    update invalid dependency

--- lib/feedbacks/visibility.feedback.ts
+++ lib/feedbacks/visibility.feedback.ts
@@ -1,14 +1,14 @@
-import { IReflectNodeReference } from "@bridged.xyz/design-sdk/lib/nodes";
+import { IReflectNodeReference } from "@design-sdk/core/nodes/lignt";
 import { ReflectLintFeedback } from "./feedback";
 
 export class VisibilityFeedback extends ReflectLintFeedback {
-    constructor(target: IReflectNodeReference, message: string) {
-        super(target, message);
-    }
+  constructor(target: IReflectNodeReference, message: string) {
+    super(target, message);
+  }
 }
 
 export class InvisibleContentWarning extends VisibilityFeedback {
-    constructor(target: IReflectNodeReference, message: string) {
-        super(target, message);
-    }
+  constructor(target: IReflectNodeReference, message: string) {
+    super(target, message);
+  }
 }
